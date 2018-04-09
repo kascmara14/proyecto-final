6332585434c55b0fcb9e8cec41626f7cb6f68db8
kascmara14 <38122418+kascmara14@users.noreply.github.com>
2018-04-08 18:24:08 -0700

    subida inicial del proyecto

---
+++ New folder/katherine program/katherine program/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace katherine_program
+{
+    class Program
+    {
+       
+        }
+        static void Main(string[] args
+          
+        
+        {
+            int a = 0, b = 0, c = 0, i=0;
+    
+                i=20;
+                    do{cout<<i<<end1;
+                        i++;
+                
+
+
+
+    while(i<20);
+
+
+
+        Console.Write("ESCRIBA LA CANTIDAD DE PERSONA EN LA SECCION ZONA NORTE" );
+                a= int.Parse(Console.ReadLine());
+            
+            Console.Write("ESCRIBA LA CANTIDAD DE PERSONA EN LA SECCION ZONA SUR" );
+                b=int.Parse(Console.ReadLine());
+            
+            Console.Write("ESCRIBA LA CANTIDAD DE PERSONA EN LA SECCION CENTRAL" );
+                c=int.Parse(Console.ReadLine());
+
+                if (a > b)
+                    if (a > c)
+                        Console.WriteLine("LA MAYOR CANTIDAD DE PERSONA ASISTIDA ESTA EN LA SECCION NORTE  {0}", a);
+                    
+                    else 
+                        if (c > b)
+                        Console.WriteLine("LA MAYOR CANTIDAD DE PERSONA ASISTIDA ESTA EN LA SECCION CENTRAL  {0}", c);
+                    else 
+                        Console.WriteLine("LA CANTIDAD DE PERSONA ASISTIDA ESTA EN LA ZONA SUR  {0}", b);
+
+                else
+
+         if (b > c) Console.WriteLine("LA MAYOR CANTIDAD DE PERSONA ASISTIDA ESTA EN LA SECCION SUR  {0}", b);
+
+                else 
+             Console.WriteLine("LA CANTIDAD DE PERSONA ASISTIDA ESTA EN LA SECCION CENTRAL  {0}",c);
+
+
+            
+
+                
+                   
+
+
+                }
+             
+
+
+
+                Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
